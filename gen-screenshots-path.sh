95a0af7f5728e2241efa0b438a321c1ee9e697ce
ChaseChoi <caiguihao@gmail.com>
2019-02-20 16:19:49 +0800

    Add function: Return to previous working directory

--- gen-screenshots-path.sh
+++ gen-screenshots-path.sh
@@ -4,13 +4,14 @@
 
 desktopPath="$HOME/Desktop"
 # Filename
-targetInfoFile="currentImgFolder.info"
-logFile="imageOperations.log"
+currentImgFolderInfoFile="currentImgFolder.log"
+operationLogFile="imageOperations.log"
 # Message
 copied="Copied to clipboard successfully!"
 failToUndo="Fail to undo!"
 screenshotNotFound="Screenshot Not Found!"
-emptyTargetInfoFile="Please define working directory first!"
+emptyWorkingDir="Please define working directory first!"
+noPreviousWorkingDir="No previous working directory!"
 separator='-'
 
 # Auxiliary Functions
@@ -33,43 +34,66 @@ errorInfo() {
     fi
 }
 
-# Check existence of targetInfoFile
-checkTargetInfoFile() {
-    if [[ ! -e ${targetInfoFile} ]]; then
-        errorInfo "${emptyTargetInfoFile}"
+# Check existence of currentImgFolderInfoFile
+checkCurrentImgFolderInfo() {
+    if [[ ! -e "${currentImgFolderInfoFile}" ]]; then
+        errorInfo "${emptyWorkingDir}"
         exit 1
     fi
 }
 
 
-
 checkLogFile() {
-    if [[ ! -e ${logFile} ]]; then
+    if [[ ! -e ${operationLogFile} ]]; then
         errorInfo "${failToUndo}"
         exit 1
     fi
 }
 
+writeLog() {
+    if [[ $# -ne 2 ]]; then
+        echo -e "\033[1;31m -- writeLog() parameter Lost! -- \033[0m"
+        exit 1
+    else
+        echo $1 >> $2
+    fi
+}
+
+deleteLastLog() {
+    sed -i '' '$ d' "$1"
+}
+
+# -d
 definePath() {
     imageFolderPath=$1
     # store info
-    echo $imageFolderPath > $targetInfoFile
+    writeLog "$imageFolderPath" "$currentImgFolderInfoFile"
+    # Write a separator
+    writeLog '-' "${operationLogFile}"
     displayWorkingDir
-    # Wring empty string
-    writeLog '-'
 }
+# -c
 displayWorkingDir() {
-    checkTargetInfoFile
-    workingDir=`cat ${targetInfoFile}`
-    echo -e "\033[1;36;40m Current image folder: $workingDir \033[0m"
+    checkCurrentImgFolderInfo
+    currentImgFolder=`tail -n 1 "${currentImgFolderInfoFile}"`
+    if [[ -d "${currentImgFolder}" ]]; then
+        echo -e "\033[1;36;40m Current image folder: ${currentImgFolder} \033[0m"
+    else
+        errorInfo "${emptyWorkingDir}"
+    fi
 }
-
+# -o
 openWorkingDir() {
-    checkTargetInfoFile
-    workingDir=`cat ${targetInfoFile}`
-    open ${workingDir}
+    checkCurrentImgFolderInfo
+    currentImgFolder=`tail -n 1 "${currentImgFolderInfoFile}"`
+    if [[ -d "${currentImgFolder}" ]]; then
+        open "${currentImgFolder}"
+    else
+        errorInfo "${emptyWorkingDir}"
+    fi
+    
 }
-
+# -h
 helpInfo() {
     echo "Usage:"
     echo -e "    cmd -d working-directory \t Define working directory."
@@ -77,39 +101,43 @@ helpInfo() {
     echo -e "    cmd -c \t Check working directory."
     echo -e "    cmd -o \t Open working directory."
     echo -e "    cmd -u \t Undo: Move image back to Desktop."
+    echo -e "    cmd -r \t Return to previous working directory."
     echo
 }
 
-writeLog() {
-    if [[ -z $1 ]]; then
-        echo -e "\033[1;31m -- writeLog() parameter Lost! -- \033[0m"
-        exit 1
-    else
-        echo $1 >> ${logFile}
-    fi
-}
-
-deleteLastLog() {
-    sed -i '' '$ d' ${logFile}
-}
-
+# -u
 undoFunction() {
     checkLogFile
-    previousImage=`tail -n 1 ${logFile}`
+    previousImage=`tail -n 1 ${operationLogFile}`
     
-    if [[ "${previousImage}" = "${separator}" || ! -e "${previousImage}" ]]; then
+    if [[ ! -e "${previousImage}" ]]; then
         errorInfo "${failToUndo}"
         exit 1
     else
         mv "${previousImage}" "${desktopPath}"
-        deleteLastLog
+        deleteLastLog "${operationLogFile}"
         successInfo "Move ${previousImage} back to ${desktopPath}"
     fi
     
 }
 
+# -r
+returnToPreDir() {
+    checkCurrentImgFolderInfo
+    lineNumber=`wc -l "${currentImgFolderInfoFile}" | awk '{print $1}'`
+    previousWorkingDir=`tail -n 2 "${currentImgFolderInfoFile}" | head -n 1`
+    
+    if [[ -d "${previousWorkingDir}" && "${lineNumber}" -gt 1 ]]; then
+        deleteLastLog "${currentImgFolderInfoFile}"
+        displayWorkingDir
+    else
+        # directory deleted OR no previous log
+        errorInfo "${noPreviousWorkingDir}"
+    fi
+}
+
 # Handle options and arguments
-while getopts ":d:hcou" opt; do
+while getopts ":d:hcoru" opt; do
     case ${opt} in
         d )
             definePath "$OPTARG"
@@ -125,6 +153,10 @@ while getopts ":d:hcou" opt; do
         ;;
         o )
             openWorkingDir
+            exit 0
+        ;;
+        r )
+            returnToPreDir
             exit 0
         ;;
         u )
@@ -143,7 +175,15 @@ while getopts ":d:hcou" opt; do
 done
 shift $((OPTIND -1))
 
+if [[ $# -gt 0 ]]; then
+    helpInfo
+    exit 1
+fi
+
 displayWorkingDir
+lastRecord=`tail -n 1 "${currentImgFolderInfoFile}"`
+# "currentImgFolder" may end with "/"
+currentImgFolder=${lastRecord%/}
 
 imageList=`find ${desktopPath} \( -name "*.png" -o -name "*.jpg" -o -name "*.gif" \) -atime -1h`
 # Screenshot not found
@@ -154,13 +194,13 @@ else
     image=`ls -t $desktopPath | egrep '\.png$|\.jpg$|\.gif$' | head -n 1`
     
     # Move to the target directory
-    mv "$desktopPath/${image}" "${workingDir}"
-    writeLog "${workingDir}/${image}"
+    mv "$desktopPath/${image}" "${currentImgFolder}"
+    writeLog "${currentImgFolder}/${image}" "${operationLogFile}"
     
     # Construct markdown image path
     imageName=`basename ${image}`
-    parentDirectory=${workingDir##*/}
-    relativePath=${parentDirectory}/${imageName}
+    parentDirectory=${currentImgFolder##*/}
+    relativePath="${parentDirectory}/${imageName}"
     # Remove file extension
     extractedName=${imageName%.*}
     stmt="![${extractedName}](${relativePath})"
@@ -168,4 +208,3 @@ else
     echo ${stmt} | tee pbcopy
     successInfo "${copied}"
 fi
-
