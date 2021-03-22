fc084836ac3ffc74c0427a2eca310b73363bdcd2
ChaseChoi <caiguihao@gmail.com>
2021-03-22 21:55:09 +0800

    Add script to filter out locked files on target directory

---
+++ filter-locked-files.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ $# -eq 1 ]]; then
+    timestamp=`date +%H_%M_%S`
+    cd "$1"
+    locked_files=`ls -lO | tr -s " " | cut -d " " -f 5,10 | egrep "uchg" | cut -d " " -f 2`
+
+    mkdir $timestamp
+    for file in $locked_files; do
+        # remove uchg flag
+        chflags nouchg $file
+        mv $file $timestamp
+        echo "Move $file to $timestamp"
+    done
+fi
